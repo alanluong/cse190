fabbc48097677dc523442d12662b2bde75eaddc1
Sara Millar <snmillar@ucsd.edu>
2014-02-07 12:49:33 -0800

    added browse activity and layout and linked it to the home page

--- src/com/example/bidit/MainActivity.java
+++ src/com/example/bidit/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.bidit;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,22 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View view) {
+		if(view instanceof Button){
+			Button clicked = (Button)view;
+			switch(clicked.getId()){
+			case R.id.buybutton:
+				Intent intent = new Intent(MainActivity.this, BrowseActivity.class);
+				startActivity(intent);
+				break;
+			case R.id.sellbutton:
+				break;
+			}
+		}
+		
 	}
 
 }
