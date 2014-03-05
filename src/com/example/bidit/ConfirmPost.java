072c9786c83ab882d6f40287ddf0cca5631e5471
Jonathan Masehi-Lano <jmasehil@ucsd.edu>
2014-03-05 01:21:20 -0800

    Cleaned working upload

--- src/com/example/bidit/ConfirmPost.java
+++ src/com/example/bidit/ConfirmPost.java
@@ -7,6 +7,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +29,13 @@ import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 
 
+import org.apache.http.HttpVersion;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.CoreProtocolPNames;
+
 import com.example.bidit.util.SystemUiHider;
 
 import android.annotation.SuppressLint;
@@ -43,6 +51,7 @@ import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -100,6 +109,10 @@ public class ConfirmPost extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+		StrictMode.setThreadPolicy(policy);
 		
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_confirm_post);
@@ -267,6 +280,160 @@ public class ConfirmPost extends Activity {
 	};
 	
 	
+	
+	
+	
+	
+	/*
+	public int uploadFile(String sourceFileUri) {
+        
+        
+        String fileName = sourceFileUri;
+
+        HttpURLConnection conn = null;
+        DataOutputStream dos = null;  
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024; 
+        File sourceFile = new File(sourceFileUri); 
+        
+        
+        if (!sourceFile.isFile()) {
+             
+             dialog.dismiss(); 
+              
+             Log.e("uploadFile", "Source File not exist :"
+                                 +uploadFilePath + "" + uploadFileName);
+              
+             runOnUiThread(new Runnable() {
+                 public void run() {
+                     messageText.setText("Source File not exist :"
+                             +uploadFilePath + "" + uploadFileName);
+                 }
+             }); 
+              
+             return 0;
+          
+        }
+        else
+        {
+        
+             try { 
+                  
+                   // open a URL connection to the Servlet
+                 FileInputStream fileInputStream = new FileInputStream(sourceFile);
+                 URL url = new URL(upLoadServerUri);
+                  
+                 // Open a HTTP  connection to  the URL
+                 conn = (HttpURLConnection) url.openConnection(); 
+                 conn.setDoInput(true); // Allow Inputs
+                 conn.setDoOutput(true); // Allow Outputs
+                 conn.setUseCaches(false); // Don't use a Cached Copy
+                 conn.setRequestMethod("POST");
+                 conn.setRequestProperty("Connection", "Keep-Alive");
+                 conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+                 conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                 conn.setRequestProperty("uploaded_file", fileName); 
+                  
+                 dos = new DataOutputStream(conn.getOutputStream());
+        
+                 dos.writeBytes(twoHyphens + boundary + lineEnd); 
+                 dos.writeBytes("Content-Disposition: form-data; name="uploaded_file";filename=""
+                                           + fileName + """ + lineEnd);
+                  
+                 dos.writeBytes(lineEnd);
+        
+                 // create a buffer of  maximum size
+                 bytesAvailable = fileInputStream.available(); 
+        
+                 bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                 buffer = new byte[bufferSize];
+        
+                 // read file and write it into form...
+                 bytesRead = fileInputStream.read(buffer, 0, bufferSize);  
+                    
+                 while (bytesRead > 0) {
+                      
+                   dos.write(buffer, 0, bufferSize);
+                   bytesAvailable = fileInputStream.available();
+                   bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                   bytesRead = fileInputStream.read(buffer, 0, bufferSize);   
+                    
+                  }
+        
+                 // send multipart form data necesssary after file data...
+                 dos.writeBytes(lineEnd);
+                 dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+        
+                 // Responses from the server (code and message)
+                 serverResponseCode = conn.getResponseCode();
+                 String serverResponseMessage = conn.getResponseMessage();
+                   
+                 Log.i("uploadFile", "HTTP Response is : "
+                         + serverResponseMessage + ": " + serverResponseCode);
+                  
+                 if(serverResponseCode == 200){
+                      
+                     runOnUiThread(new Runnable() {
+                          public void run() {
+                               
+                              String msg = "File Upload Completed.\n\n See uploaded file here : \n\n"
+                                            +" http://www.androidexample.com/media/uploads/"
+                                            +uploadFileName;
+                               
+                              messageText.setText(msg);
+                              Toast.makeText(UploadToServer.this, "File Upload Complete.", 
+                                           Toast.LENGTH_SHORT).show();
+                          }
+                      });                
+                 }    
+                  
+                 //close the streams //
+                 fileInputStream.close();
+                 dos.flush();
+                 dos.close();
+                   
+            } catch (MalformedURLException ex) {
+                 
+                dialog.dismiss();  
+                ex.printStackTrace();
+                 
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        messageText.setText("MalformedURLException Exception : check script url.");
+                        Toast.makeText(UploadToServer.this, "MalformedURLException", 
+                                                            Toast.LENGTH_SHORT).show();
+                    }
+                });
+                 
+                Log.e("Upload file to server", "error: " + ex.getMessage(), ex);  
+            } catch (Exception e) {
+                 
+                dialog.dismiss();  
+                e.printStackTrace();
+                 
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        messageText.setText("Got Exception : see logcat ");
+                        Toast.makeText(UploadToServer.this, "Got Exception : see logcat ", 
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+                Log.e("Upload file to server Exception", "Exception : "
+                                                 + e.getMessage(), e);  
+            }
+            dialog.dismiss();       
+            return serverResponseCode; 
+             
+         //} // End else block 
+       }
+	*/
+    
+
+	
 	private void postItem(String path)
 	{		
          
@@ -302,24 +469,31 @@ public class ConfirmPost extends Activity {
 
 	         post.setEntity(builder.build()); 
 
-	         try {
+	         try 
+	         {
 				HttpResponse response = client.execute(post);
-			} catch (ClientProtocolException e) {
+	         } 
+	         
+	         catch (ClientProtocolException e) 
+	         {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			} catch (IOException e) {
+	         } 
+	         
+	         catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}
-			return null;  
+	         }
+	         return null;  
 	    }
 
 	    protected void onPostExecute() {
 	        // TODO: check this.exception 
 	        // TODO: do something with the feed
 	    }
-	
+
 	}
+	
 
 	
 
