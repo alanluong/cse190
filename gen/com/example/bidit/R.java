347a1a19d13d8e0fe43fe9a90634e77ea536ee15
gzyang <gzyang@ucsd.edu>
2014-03-05 19:34:24 -0800

    bid layout; not finished

--- gen/com/example/bidit/R.java
+++ gen/com/example/bidit/R.java
@@ -59,14 +59,15 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int textView2=0x7f09000d;
     }
     public static final class layout {
-        public static final int activity_browse=0x7f030000;
-        public static final int activity_confirm_post=0x7f030001;
-        public static final int activity_main=0x7f030002;
-        public static final int activity_message=0x7f030003;
-        public static final int activity_post=0x7f030004;
-        public static final int activity_sell=0x7f030005;
-        public static final int activity_splash=0x7f030006;
-        public static final int ads_list_item=0x7f030007;
+        public static final int activity_bids=0x7f030000;
+        public static final int activity_browse=0x7f030001;
+        public static final int activity_confirm_post=0x7f030002;
+        public static final int activity_main=0x7f030003;
+        public static final int activity_message=0x7f030004;
+        public static final int activity_post=0x7f030005;
+        public static final int activity_sell=0x7f030006;
+        public static final int activity_splash=0x7f030007;
+        public static final int ads_list_item=0x7f030008;
     }
     public static final class menu {
         public static final int main=0x7f080000;
