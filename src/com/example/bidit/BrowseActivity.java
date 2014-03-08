26c0559e9cd0f32b6eee96c18b2e2629cdadf219
Sara Millar <snmillar@ucsd.edu>
2014-03-08 14:03:27 -0800

    added back button to browse activity

--- src/com/example/bidit/BrowseActivity.java
+++ src/com/example/bidit/BrowseActivity.java
@@ -12,23 +12,17 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.app.Activity;
-import android.app.ListActivity;
+import android.app.ActionBar;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.Button;
-import android.widget.ListView;
+import android.view.MenuItem;
 
-public class BrowseActivity extends BiditActivity implements OnClickListener {
+public class BrowseActivity extends BiditActivity {
 
 	AdAdapter adapter;
 
@@ -36,6 +30,10 @@ public class BrowseActivity extends BiditActivity implements OnClickListener {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_browse);
+		
+		ActionBar actionBar = getActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
+		
 		adapter = new AdAdapter(getSupportFragmentManager());
 		
 		// This is for ListView
@@ -106,10 +104,11 @@ public class BrowseActivity extends BiditActivity implements OnClickListener {
 			Log.d(BrowseActivity.class.getName(), "" + adapter.getCount());
 		}
 	}
-
-
+	
 	@Override
-	public void onClick(View v) {
-		
+	public boolean onOptionsItemSelected(MenuItem item){
+		Intent myIntent = new Intent(getApplicationContext(), MainActivity.class);
+		startActivityForResult(myIntent, 0);
+		return true;
 	}
 }
